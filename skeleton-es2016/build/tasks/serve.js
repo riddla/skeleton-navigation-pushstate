61b57777a5d99832df45b6600752abe9bb50f75d
Volker Rose <volker.rose@gmail.com>
2016-05-20 13:02:15 +0200

    feat(skeleton-es2016): use connect-history-api-fallback in gulp serve task

--- skeleton-es2016/build/tasks/serve.js
+++ skeleton-es2016/build/tasks/serve.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
@@ -11,10 +12,13 @@ gulp.task('serve', ['build'], function(done) {
     port: 9000,
     server: {
       baseDir: ['.'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
+      middleware: [
+        historyApiFallback(),
+        function (req, res, next) {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        }
+      ]
     }
   }, done);
 });
@@ -29,10 +33,13 @@ gulp.task('serve-bundle', ['bundle'], function(done) {
     port: 9000,
     server: {
       baseDir: ['.'],
-      middleware: function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
+      middleware: [
+        historyApiFallback(),
+        function (req, res, next) {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        }
+      ]
     }
   }, done);
 });
